1a43da41d460ee48f6a8807f2f6d6df1530b618b
Nyasha Musanhu <danielsnyasha@outlook.com>
2022-06-09 13:53:01 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const db = mysql.createConnection({
   host: "192.168.0.2",
   port: "1212",
   user: "DevUser",
-  password:"lcv.120197",
+  password:"********",
   // password: "lcv.120197",
   database: "lcvsscopus_data",
 
